0ae8299a764802c6d4d5232e1d93f8d49e180f6b
Oleksii Mishchenko <alex3hard@gmail.com>
2026-01-11 13:44:46 +0200

    feat Login/logout

--- it_task_manager/settings.py
+++ it_task_manager/settings.py
@@ -121,3 +121,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 AUTH_USER_MODEL = "manager.Worker"
+
+LOGIN_REDIRECT_URL = "/"
+
+LOGOUT_REDIRECT_URL = "/accounts/login/"
---
+++ manager/forms/worker_form.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from manager.models import Worker
+
+class WorkerCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Worker
+        fields = UserCreationForm.Meta.fields + (
+            "first_name",
+            "last_name",
+            "email",
+            "position",
+        )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = (
+                'input input-bordered w-full italic font-semibold focus:border-primary'
+            )
