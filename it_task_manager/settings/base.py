cbffaeee0c2cc55cbb97496d2c0005599641aa2d
Oleksii Mishchenko <alex3hard@gmail.com>
2026-01-11 21:11:10 +0200

    rearranged settings in 3 files, made dump.json

--- it_task_manager/settings/base.py
+++ it_task_manager/settings/base.py
@@ -13,7 +13,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -21,12 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KET", "django-insecure-t8=^xxsgp1zi&9dr&-0hh)aq8l!$%d5a$u#-_97+rr63)c$%el")
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
-
-ALLOWED_HOSTS = ["127.0.0.1"]
-
 
 # Application definition
 
@@ -73,18 +67,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "it_task_manager.wsgi.application"
-
-
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
